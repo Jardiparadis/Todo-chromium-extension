bad21223352fda394ef3c7145f05b18ff8f5a87a
Justin FOURNIER <justin.fournier@epitech.eu>
2025-02-15 18:42:12 +0100

    delete task

--- panel/panel.js
+++ panel/panel.js
@@ -1,8 +1,7 @@
 function createTask(_taskName, _id) {
     const taskName = typeof _taskName === "string" ?
         _taskName : document.getElementById("create-task-input").value;
-    const id = typeof _id === "number" ?
-        _id : document.querySelectorAll(".task").length;
+    const id = typeof _id === "string" ? _id : 'id' + (new Date()).getTime();
 
     let newTaskDiv = document.createElement("div");
     newTaskDiv.className = "form-check task";
@@ -14,33 +13,40 @@ function createTask(_taskName, _id) {
             </label>
             <input class="form-check-input" id="checkbox-task-${id}" type="checkbox" />
         </div>
-        <button class="btn btn-danger btn-sm" style="margin-left: auto" type="button" title="Delete task">
+        <button class="btn btn-danger btn-sm" style="margin-left: auto" type="button" title="Delete task" id="delete-${id}">
             <i class="bi bi-trash3-fill"></i>
         </button>
     `;
     document.getElementById("tasks-list").appendChild(newTaskDiv);
+    document.getElementById("delete-" + id).addEventListener("click", deleteButtonClickHandler(id));
 
     // Reset input
     document.getElementById("create-task-input").value = "";
 
     // If we are not adding task from the local storage
     if (typeof _taskName !== "string") {
-        updateLocalStorage(taskName);
+        // Then we add it to the local storage
+        const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+        tasks.push({
+            name : taskName,
+            id   : id
+        });
+        localStorage.setItem("tasks", JSON.stringify(tasks));
     }
-
 }
 
-function updateLocalStorage(taskNameToAdd) {
-    const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-    tasks.push(taskNameToAdd);
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+const deleteButtonClickHandler = (taskIdToDelete) => {
+    return function(_event){
+        document.getElementById("task-" + taskIdToDelete).remove();
+        const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+        localStorage.setItem("tasks", JSON.stringify(tasks.filter(el => el.id !== taskIdToDelete)));
+    }
 }
 
 function loadTasksFromLocalStorage() {
     const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-    let id = 0;
     for (const task of tasks) {
-        createTask(task, id++);
+        createTask(task.name, task.id);
     }
 }
 
