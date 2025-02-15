fc4cae6035008d508dbdaab0b669c2f21e1e7200
Justin FOURNIER <justin.fournier@epitech.eu>
2025-02-15 19:04:45 +0100

    allow tasks to be completed

--- panel/panel.js
+++ panel/panel.js
@@ -1,7 +1,8 @@
-function createTask(_taskName, _id) {
+function createTask(_taskName, _id, _isCompleted) {
     const taskName = typeof _taskName === "string" ?
         _taskName : document.getElementById("create-task-input").value;
     const id = typeof _id === "string" ? _id : 'id' + (new Date()).getTime();
+    const isCompleted = _isCompleted || false;
 
     let newTaskDiv = document.createElement("div");
     newTaskDiv.className = "form-check task";
@@ -9,7 +10,9 @@ function createTask(_taskName, _id) {
     newTaskDiv.innerHTML = `
         <div>
             <label class="form-check-label task-label" for="task-${id}">
+                ${isCompleted === true ? "<del>" : ""}
                 ${taskName}
+                ${isCompleted === true ? "</del>" : ""}
             </label>
             <input class="form-check-input" id="checkbox-task-${id}" type="checkbox" />
         </div>
@@ -19,6 +22,8 @@ function createTask(_taskName, _id) {
     `;
     document.getElementById("tasks-list").appendChild(newTaskDiv);
     document.getElementById("delete-" + id).addEventListener("click", deleteButtonClickHandler(id));
+    document.getElementById("checkbox-task-" + id).addEventListener("click", completeTaskButtonClickHandler(id));
+    document.getElementById("checkbox-task-" + id).checked = isCompleted;
 
     // Reset input
     document.getElementById("create-task-input").value = "";
@@ -28,10 +33,25 @@ function createTask(_taskName, _id) {
         // Then we add it to the local storage
         const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
         tasks.push({
-            name : taskName,
-            id   : id
+            name      : taskName,
+            id        : id,
+            isCompleted : isCompleted
         });
         localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+}
+
+const completeTaskButtonClickHandler = (taskIdToComplete) => {
+    return function(_event){
+        const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+        for (const task of tasks) {
+            if (task.id === taskIdToComplete) {
+                task.isCompleted = document.getElementById("checkbox-task-" + taskIdToComplete).checked;
+                break;
+            }
+        }
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        loadTasksFromLocalStorage()
     }
 }
 
@@ -45,8 +65,9 @@ const deleteButtonClickHandler = (taskIdToDelete) => {
 
 function loadTasksFromLocalStorage() {
     const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+    document.getElementById("tasks-list").innerHTML = "";
     for (const task of tasks) {
-        createTask(task.name, task.id);
+        createTask(task.name, task.id, task.isCompleted);
     }
 }
 
